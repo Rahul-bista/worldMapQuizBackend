6674d5072733f3e9fccf4ed160d6ed4ae5c69839
Rahul-bista <rahulbista850@gmail.com>
2024-09-13 04:57:48 +0545

    added apps & rest_framework

--- worldMapQuiz/settings.py
+++ worldMapQuiz/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'worldMapQuiz',
+    'guessTheFlag',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
